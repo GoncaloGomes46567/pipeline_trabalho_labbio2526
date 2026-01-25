637f0000c5efca4de4eda39b790b6e76d2d676c8
GoncaloGomes46567 <goncalogomes999@outlook.pt>
2026-01-25 11:00:06 +0000

    update blast_phylo.py to improve performance and fix bugs

--- src/blast_phylo.py
+++ src/blast_phylo.py
@@ -10,20 +10,20 @@ def aplica_blast(fasta_file, output_dir):
 	print(f"\nA iniciar o BLASTP remoto para {len(record.seq)} aa... (Aguarde)")
 	
 	try:
-	    result_handle = NCBIWWW.qblast("blastp", "nr", record.format("fasta), hitlist_size = 20)
-	    xml_file = os.path.join(output_dir, "blast_results.xml")
-	    
-	    with open(xml_file, "w") as f:
-		 f.write(result_handle.read())
-	  
-            result_handle.close()
-	    print(f"Resultados do BLAST salvos em: {xml_file}")
-	    return xml_file
+		result_handle = NCBIWWW.qblast("blastp", "nr", record.format("fasta"), hitlist_size=20)
+		xml_file = os.path.join(output_dir, "blast_results.xml")
+		
+		with open(xml_file, "w") as f:
+			f.write(result_handle.read())
+		
+		result_handle.close()
+		print(f"Resultados do BLAST salvos em: {xml_file}")
+		return xml_file
 	except Exception as e:
-	      print(f"Erro no BLAST: {e}")
-	      return None
+		print(f"Erro no BLAST: {e}")
+		return None
 
-def processar_blast_e_salvar_hits)xml_file, output_dir):
+def processar_blast_e_salvar_hits(xml_file, output_dir):
 	try:
 		blast_record = NCBIXML.read(open(xml_file))
 		if len(blast_record.alignments) == 0:
@@ -31,11 +31,12 @@ def processar_blast_e_salvar_hits)xml_file, output_dir):
 			return None
 		print(f" Foram encontrados {len(blast_record.alignments)} alinhamentos.")
 		print(f" Top hit: {blast_record.alignments[0].title[:60]}...")
-		hits_fasta = os.path.join(output_dir, "blast_hits.fasta)
+		hits_fasta = os.path.join(output_dir, "blast_hits.fasta")
 		with open(hits_fasta, "w") as f:
-		     for alignment.hsps[0]
-		     clean_id = alignment.accession.replace(" ", "_")
-		     f.write(f">{clean_id}\n{hsp.sbjct}\n")
+			for alignment in blast_record.alignments:
+				hsp = alignment.hsps[0]
+				clean_id = alignment.accession.replace(" ", "_")
+				f.write(f">{clean_id}\n{hsp.sbjct}\n")
 
 		print(f"Sequências dos hits salvas em: {hits_fasta}")
 		return hits_fasta
@@ -57,10 +58,8 @@ def gerar_arvore_simples(hits_fasta, output_dir):
 		axes = plt.axes(frameon = False)
 		Phylo.draw(tree, axes = axes, do_show = False)
 		png_file = os.path.join(output_dir, "arvore_filogenetica.png")
-		plt.saveig(png_file)
+		plt.savefig(png_file)
 		print(f"Árvore salva (imagem): {png_file}")
 		print(f"Árvore salva(dados): {tree_file}")
 	except Exception as e:
 		print(f"Erro ao gerar a árvore: {e}")
-		
-	
