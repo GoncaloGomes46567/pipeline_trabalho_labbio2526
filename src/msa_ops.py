57c24b0ced77999650b33cca046d07862b48c828
GoncaloGomes46567 <goncalogomes999@outlook.pt>
2026-01-25 12:09:52 +0000

    update msa_ops.py to reflect simplified MUSCLE command usage

--- src/msa_ops.py
+++ src/msa_ops.py
@@ -4,23 +4,26 @@ import subprocess
 import os
 import subprocess
 
-def realizar_alinhamento_clustalw(input_fasta, output_dir, clustalw_exe="clustalw2"):
+def realizar_alinhamento_msa(input_fasta, output_dir, muscle_exe="muscle"):
+    """
+    Realiza alinhamento múltiplo de sequências usando o MUSCLE.
+    """
     output_aln = os.path.join(output_dir, "alinhamento.aln")
-
-    print(f"\n--- Iniciando o Alinhamento Múltiplo (ClustalW) ---")
+    
+    print(f"\n--- Iniciando o Alinhamento Múltiplo (MUSCLE) ---")
     
     if not os.path.exists(input_fasta):
         print(f"Erro: Arquivo {input_fasta} não encontrado.")
         return None
 
     comando = [
-        clustalw_exe,
-        f"-INFILE={input_fasta}",
-        f"-OUTFILE={output_aln}",
-        "-OUTPUT=CLUSTAL"
+        muscle_exe,
+        "-in", input_fasta,
+        "-out", output_aln
     ]
 
     try:
+        
         resultado = subprocess.run(comando, capture_output=True, text=True)
         
         if resultado.returncode == 0:
@@ -28,12 +31,11 @@ def realizar_alinhamento_clustalw(input_fasta, output_dir, clustalw_exe="clustal
             print(f"Arquivo salvo em: {output_aln}")
             return output_aln
         else:
-            print(f"Erro no ClustalW: {resultado.stderr}")
+            print(f"Erro no MUSCLE: {resultado.stderr}")
             return None
             
     except FileNotFoundError:
-        print(f"Erro: O executável '{clustalw_exe}' não foi encontrado.")
-        print("Certifique-se de que o ClustalW2 está instalado e no PATH do Windows.")
+        print(f"Erro: O executável '{muscle_exe}' não foi encontrado na pasta ou no PATH.")
         return None
     except Exception as e:
         print(f"Erro inesperado: {e}")
