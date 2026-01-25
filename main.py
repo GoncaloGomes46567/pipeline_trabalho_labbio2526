570c2caccb2a401ffa4fca11c025b42d529a3440
GoncaloGomes46567 <goncalogomes999@outlook.pt>
2026-01-25 11:42:12 +0000

    update main.py to make email and gene_id optional and use inputs() function if not provided via command line arguments

--- main.py
+++ main.py
@@ -3,46 +3,56 @@ import os
 import sys
 from src import ncbi, analisa, blast_phylo, msa_ops
 
+def inputs():
+	email = input("Digite seu email para o NCBI: ")
+	gene_id = input("Digite o ID do gene: ")
+	return email, gene_id
 
 def main():
-	parser = argparse.ArgumentParser(description = "Pipeline Genética Completa)")
-	parser.add_argument("--email", required = True, help="Email para o NCBI")
-	parser.add_argument("--gene_id", required = True, help = "ID do gene")
-	parser.add_argument("--xml_file", required=True, help= "Fucgeuri XML do BLAST")
-	parser.add_argument("--faa_file", required=True, help = "Ficheiro FASTA (.faa)")
-	parser.add_argument("--output", default = "data", help = "Pasta de saída")
-	parser.add_argument("--filter_name", default = None, help = "Filtro de nome da proteína")
-	parser.add_argument("--clustal_path", default = "clustalw2", help = "Path para o executável do ClustalW. Se instalado no sistema, deixa o padrão.")
+    parser = argparse.ArgumentParser(description="Pipeline Genética Completa")
+    
+    parser.add_argument("--email", help="Email para o NCBI")
+    parser.add_argument("--gene_id", help="ID do gene")
+    parser.add_argument("--output", default="data", help="Pasta de saída")
+    parser.add_argument("--filter_name", default=None, help="Filtro de nome da proteína")
+    parser.add_argument("--clustal_path", default="clustalw2", help="Caminho do ClustalW")
 
-	args = parser.parse_args()
-	if not os.path.exists(args.output):
-		os.makedirs(args.output, exist_ok = True)
+    args = parser.parse_args()
 
-	ncbi.setup_entrez(args.email)
+    
+    if not args.email or not args.gene_id:
+        args.email, args.gene_id = inputs()
 
-	gb_file,  _ = ncbi.buscar_gene_gb(args.gene_id, args.output)
-	if not gb_file: sys.exit("erro no download.")
-	faa_file = ncbi.extrair_cds_proteina(gb_file, args.output, args.filter_name)
-	if not faa_file: sys.exit("Erro na extração")
+    if not os.path.exists(args.output):
+        os.makedirs(args.output, exist_ok=True)
 
-	analisa.analisar_propriedades(faa_file)
+    ncbi.setup_entrez(args.email)
 
-	if xml_file:
-		hits_file = blast_phylo.processar_blast_e_salvar_hits(
-        xml_file,
-        args.output,
-        faa_file
-    )
+    gb_file, _ = ncbi.buscar_gene_gb(args.gene_id, args.output)
+    if not gb_file:
+        sys.exit("erro no download.")
+    faa_file = ncbi.extrair_cds_proteina(gb_file, args.output, args.filter_name)
+    if not faa_file:
+        sys.exit("Erro na extração")
 
-	if hits_file:
-		aln_file = msa_ops.realizar_alinhamento_clustalw(
+    analisa.analisar_propriedades(faa_file)
+
+    if xml_file:
+        hits_file = blast_phylo.processar_blast_e_salvar_hits(
+            xml_file,
+            args.output,
+            faa_file
+        )
+
+    if hits_file:
+        aln_file = msa_ops.realizar_alinhamento_clustalw(
             hits_file,
             args.output,
             args.clustal_path
         )
 
-	if aln_file:
-            blast_phylo.gerar_arvore_phylo(aln_file, args.output)
+    if aln_file:
+        blast_phylo.gerar_arvore_phylo(aln_file, args.output)
 
 if __name__ == "__main__":
 	main()
